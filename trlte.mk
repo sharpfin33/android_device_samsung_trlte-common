0ed0841fbdd83fb4b604a4d8c6b39fe2ed00bd91
arco <arco68@gmail.com>
2020-02-13 17:34:33 +0100

    trlte-common: Move to binderized lights HAL

--- trlte.mk
+++ trlte.mk
@@ -202,8 +202,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    lights.APQ8084
+    android.hardware.light@2.0-service.samsung
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
