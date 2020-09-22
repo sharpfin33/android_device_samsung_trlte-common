daf1c7ace30cea3e2e72264182cd0ca723e2d06b
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    trlte-common: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- trlte.mk
+++ trlte.mk
@@ -293,7 +293,7 @@ PRODUCT_COPY_FILES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service-qti
 
 # Radio
 PRODUCT_PACKAGES += \
