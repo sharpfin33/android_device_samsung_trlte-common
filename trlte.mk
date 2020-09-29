eaaee991a67904cd6346b0bd5a59813207517806
Bruno Martins <bgcngm@gmail.com>
2020-09-29 10:45:23 +0100

    trlte-common: Build 2.1 health HAL
    The devices fail to boot otherwise.
    
    Change-Id: I5bc6ae361a5319224b87ef6a4d7127b6b5bc9a97

--- trlte.mk
+++ trlte.mk
@@ -172,7 +172,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-   android.hardware.health@2.0-service.samsung8084
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
