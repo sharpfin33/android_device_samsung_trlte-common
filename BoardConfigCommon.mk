378782f829a0953973da465ebea95a70ef4913d6
karthick111 <karthickrko61@gmail.com>
2020-10-13 00:34:49 +0200

    trlte-common: Add MALLOC_SVELTE := true
    We are low memory by current standards.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -131,6 +131,9 @@ TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/libsec-ril.so|/system/vendor/lib/libprotobuf-cpp-full.so \
     /system/vendor/lib/libloc_eng.so|/system/lib/libshim_loc.so
 
+# Low memory devices
+MALLOC_SVELTE := true
+
 # Media
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
