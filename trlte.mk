48cf18e506a2027c972bb392e0fd47efe1f29d0a
Nicholas Magill <nmagill1979@gmail.com>
2021-04-11 17:10:06 +0100

    trlte-common: Import modified IR HAL
    from https://github.com/LineageOS/android_hardware_samsung/tree/lineage-16.0
    
    Change-Id: Icf2dcd1176c07186e1ad7da3ed21b6a32890ae6c

--- trlte.mk
+++ trlte.mk
@@ -172,7 +172,9 @@ PRODUCT_PACKAGES += \
 
 # IR Blaster
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-service.samsung
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service \
+    consumerir.apq8084
 
 # IRSC
 PRODUCT_COPY_FILES += \
