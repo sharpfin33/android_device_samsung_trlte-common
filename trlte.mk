6eb4d5342916515dd000014186d668336fd33459
haggertk <haggertk@lineageos.org>
2021-11-15 13:55:24 -0500

    trlte-common: Use libprotobuf-cpp-lite-v29.so for libwvdrmengine.so * libwvdrmengine.so needs _ZN6google8protobuf8internal13empty_string_E   which is dropped in the API 30 library, but can be found in the   API 29 prebuilt

--- trlte.mk
+++ trlte.mk
@@ -344,6 +344,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vndk_package
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
+
 # TimeKeep
 PRODUCT_PACKAGES += \
     timekeep \
