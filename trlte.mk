9bf736a7928b2948fcccc89b8a22ab47a3190f88
Flominator <github.flominatorgd@gmail.com>
2022-09-15 01:55:35 +0200

    trlte-common: built android.hardware.power.stats@1.0-service.mock [1/2]

--- trlte.mk
+++ trlte.mk
@@ -321,7 +321,8 @@ PRODUCT_COPY_FILES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Radio
 PRODUCT_PACKAGES += \
