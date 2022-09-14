e2194e2bdac019c7ab0b38afce362aa17689089c
Flominator <github.flominatorgd@gmail.com>
2022-09-15 01:45:12 +0200

    trlte-common: import a working light hal [1/2]
    built android.hardware.light@2.0-service.apq8084

--- trlte.mk
+++ trlte.mk
@@ -219,7 +219,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.samsung
+    android.hardware.light@2.0-service.apq8084
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
